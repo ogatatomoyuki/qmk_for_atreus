87d3ac69adb5e879da55180782a1d8949394218c
緒方 智行 <ogata.tomoyuki@gmail.com>
2025-11-22 00:34:11 +0900

    Implement custom keycode and multiple layers
    Added custom keycode GUISP for GUI + SPACE shortcut and defined multiple layers for keyboard layout.

--- keyboards/keyboardio/atreus/keymaps/default/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/default/keymap.c
@@ -1,43 +1,68 @@
-// Copyright (C) 2019, 2020  Keyboard.io, Inc
-//
-// this is the style you want to emulate.
-// This is the canonical layout file for the Quantum project. If you want to add another keyboard,
-
 #include QMK_KEYBOARD_H
 
+// カスタムキーコードの定義
+// GUISP (GUI + SPACE) は Layer 1 での LGUI(KC_SPACE) ショートカットに対応します。
+enum custom_keycodes {
+    GUISP = SAFE_RANGE, 
+};
+
+// レイヤー名の定義 (Vialの4層に対応)
 enum layer_names {
-    _QW,
-    _RS,
-    _LW,
+    _BASE = 0,
+    _ADJUST,
+    _MOUSE,
+    _NUMBERS
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QW] = LAYOUT( /* Qwerty */
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT ),
+  // レイヤー 0: _BASE (QWERTY) - Vial設定 Layer 0 に基づく
+  [_BASE] = LAYOUT(
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      XXXXXXX, XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      XXXXXXX, XXXXXXX, KC_H,    KC_J,    KC_K,    KC_L,    KC_MINUS,
+    LCTL_T(KC_Z), LSFT_T(KC_X), LALT_T(KC_C), LGUI_T(KC_V), KC_B, KC_NO, KC_NO, KC_N, KC_M, RALT_T(KC_COMM), RSFT_T(KC_DOT), RCTL_T(KC_SLSH),
+    KC_NO,   KC_NO,   LT(_ADJUST,KC_LANG2), LGUI_T(KC_SPACE), LT(_MOUSE,KC_LANG1), KC_NO, KC_NO, LT(_NUMBERS,KC_BSPC), RSFT_T(KC_ENTER), KC_NO, KC_NO, KC_NO
+  ),
 
-  /*
-   *  !       @     up     {    }        ||     pgup    7     8     9    *
-   *  #     left   down  right  $        ||     pgdn    4     5     6    +
-   *  [       ]      (     )    &        ||       `     1     2     3    \
-   * lower  insert super shift bksp ctrl || alt space   fn    .     0    =
-   */
-  [_RS] = LAYOUT( /* [> RAISE <] */
-    KC_EXLM, KC_AT,   KC_UP,   KC_DLR,  KC_PERC,                  KC_PGUP, KC_7,    KC_8,   KC_9, KC_BSPC,
-    KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_RPRN,                  KC_PGDN, KC_4,    KC_5,   KC_6, KC_BSLS,
-    KC_LBRC, KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC, KC_AMPR,KC_ASTR, KC_1,    KC_2,   KC_3, KC_PLUS,
-    TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL ),
-  /*
-   * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
-   *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
-   *       volup             reset      ||             F1    F2    F3   F12
-   *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
-   */
-  [_LW] = LAYOUT( /* [> LOWER <] */
-    KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
-    KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
-    KC_NO,   KC_VOLU, KC_NO,   KC_NO,   QK_BOOT, _______, _______, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  TO(_QW), KC_PSCR, KC_SCRL, KC_PAUS )
+  // レイヤー 1: _ADJUST (調整レイヤー) - Vial設定 Layer 1 に基づく
+  // Row 1, Col 4 に LGUI(KC_SPACE) の代わりとして GUISP を割り当て
+  [_ADJUST] = LAYOUT(
+    KC_ESCAPE, KC_NO,   KC_NO,   KC_NO,   KC_F4,                     XXXXXXX, XXXXXXX, KC_HOME, KC_PGDOWN, KC_PGUP, KC_END,  KC_NO,
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   GUISP,                     XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_NO,
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TAB,                    KC_NO,   KC_NO,   KC_NO,   KC_LBRC, KC_RBRC, KC_COMM, KC_DOT,  KC_NO,
+    KC_NO,   KC_NO,   KC_NO,   KC_LGUI, KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_RSHIFT, KC_NO,   KC_NO,   KC_NO
+  ),
+
+  // レイヤー 2: _MOUSE (マウス操作) - Vial設定 Layer 2 に基づく
+  [_MOUSE] = LAYOUT(
+    KC_NO,   KC_PWR,  KC_NO,   KC_NO,   KC_BTN1,                   XXXXXXX, XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO,
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     XXXXXXX, XXXXXXX, KC_NO,   KC_BTN1, KC_BTN2, KC_BTN3, KC_NO,
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_MS_L, KC_MS_D, KC_MS_D, KC_MS_R, KC_NO,
+    KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_NO,   KC_NO,   KC_NO
+  ),
+
+  // レイヤー 3: _NUMBERS (数字と記号) - Vial設定 Layer 3 に基づく
+  [_NUMBERS] = LAYOUT(
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      XXXXXXX, XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     XXXXXXX, XXXXXXX, KC_EQL,  KC_BSLS, KC_GRV,  KC_SCLN, KC_QUOT,
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_LBRC, KC_RBRC, KC_COMM, KC_DOT,  KC_SLSH,
+    KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_NO,   KC_NO,   KC_NO
+  )
 };
+
+// カスタムマクロ処理の定義
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case GUISP:
+            if (record->event.pressed) {
+                // キーが押されたとき: 左GUIを押してからスペースを押す (LGUI + SPACE)
+                register_code(KC_LGUI);
+                register_code(KC_SPACE);
+            } else {
+                // キーが離されたとき: スペースを離してから左GUIを離す
+                unregister_code(KC_SPACE);
+                unregister_code(KC_LGUI);
+            }
+            return false;
+    }
+    return true;
+}
