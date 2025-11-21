a9b518bd9d7f5f3aade282664398bc3195cf48ae
緒方 智行 <ogata.tomoyuki@gmail.com>
2025-11-22 02:06:11 +0900

    Update rules.mk for Vial configuration

--- keyboards/keyboardio/atreus/keymaps/default/rules.mk
+++ keyboards/keyboardio/atreus/keymaps/default/rules.mk
@@ -1,35 +1,33 @@
-QMK_SETTINGS = yes
+# Vial を使うので VIA はオフ
+VIA_ENABLE          = no
+VIAL_ENABLE         = yes
+LTO_ENABLE          = yes
 
-# --- 必要最小限 ---
-VIA_ENABLE = no
-VIAL_ENABLE = yes
-LTO_ENABLE = yes
-EEPROM_ENABLE = yes
+# Vial のために必要
+EEPROM_ENABLE       = yes
 
-# --- 重い/不要機能は全部OFF ---
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
-MOUSEKEY_ENABLE = yes
-EXTRAKEY_ENABLE = no
-NKRO_ENABLE = no
-BACKLIGHT_ENABLE = no
-RGBLIGHT_ENABLE = no
-RGB_MATRIX_ENABLE = no
-AUDIO_ENABLE = no
-MIDI_ENABLE = no
-SLEEP_LED_ENABLE = no
+# 余計な設定画面はいらない
+QMK_SETTINGS        = no
 
-# QMKの便利系も削る
-MAGIC_ENABLE = no
-BOOTMAGIC_ENABLE = no          # 必要なら _LITE を yes に（それでも重ければ no のまま）
-SPACE_CADET_ENABLE = no
-GRAVE_ESC_ENABLE = yes
-TAP_DANCE_ENABLE = no
-LEADER_ENABLE = no
-COMBO_ENABLE = no
-UNICODE_ENABLE = no
-UNICODEMAP_ENABLE = no
+# --- 必要なければ全部 no に（あとで必要になったら yes に変える） ---
+MOUSEKEY_ENABLE     = no   # マウスキー使うなら yes
+EXTRAKEY_ENABLE     = no   # メディアキーとかが欲しければ yes
+CONSOLE_ENABLE      = no
+COMMAND_ENABLE      = no
+NKRO_ENABLE         = no
+BACKLIGHT_ENABLE    = no
+RGBLIGHT_ENABLE     = no
+AUDIO_ENABLE        = no
+UNICODE_ENABLE      = no
+UNICODEMAP_ENABLE   = no
+TAP_DANCE_ENABLE    = no
+LEADER_ENABLE       = no
+COMBO_ENABLE        = no
 KEY_OVERRIDE_ENABLE = no
-CAPS_WORD_ENABLE = no
-STENO_ENABLE = no
-AUTOCORRECT_ENABLE = no
+CAPS_WORD_ENABLE    = no
+STENO_ENABLE        = no
+AUTOCORRECT_ENABLE  = no
+MAGIC_ENABLE        = no
+BOOTMAGIC_ENABLE    = no
+SPACE_CADET_ENABLE  = no
+GRAVE_ESC_ENABLE    = no   # 必要なら yes に
