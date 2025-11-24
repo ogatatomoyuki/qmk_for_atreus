3e700610744356b874cfab518cab2d3eba68016c
緒方 智行 <ogata.tomoyuki@gmail.com>
2025-11-24 12:44:03 +0900

    Update rules.mk

--- keyboards/keyboardio/atreus/keymaps/default/rules.mk
+++ keyboards/keyboardio/atreus/keymaps/default/rules.mk
@@ -1,36 +1,34 @@
-QMK_SETTINGS = yes
+# --- Vial / QMK Settings 有効 ---
+VIAL_ENABLE   = yes
+VIA_ENABLE    = no        # サイズ節約のため off（Vial だけ使う前提）
+QMK_SETTINGS  = yes       # Vial の「QMK Settings」タブを使う
 
-# --- 必要最小限 ---
-VIA_ENABLE = yes
-VIAL_ENABLE = yes
-LTO_ENABLE = yes
+# --- 最低限必要なものだけ ---
 EEPROM_ENABLE = yes
+LTO_ENABLE    = yes       # リンク時最適化（サイズ削減）
 
-# --- 重い/不要機能は全部OFF ---
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
-MOUSEKEY_ENABLE = yes
-EXTRAKEY_ENABLE = no
-NKRO_ENABLE = no
-BACKLIGHT_ENABLE = no
-RGBLIGHT_ENABLE = no
-RGB_MATRIX_ENABLE = no
-AUDIO_ENABLE = no
-MIDI_ENABLE = no
-SLEEP_LED_ENABLE = no
+# --- 重い・未使用機能はすべて無効化 ---
+CONSOLE_ENABLE       = no
+COMMAND_ENABLE       = no
+MOUSEKEY_ENABLE      = no
+EXTRAKEY_ENABLE      = no
+NKRO_ENABLE          = no
+BACKLIGHT_ENABLE     = no
+RGBLIGHT_ENABLE      = no
+RGB_MATRIX_ENABLE    = no
+AUDIO_ENABLE         = no
+MIDI_ENABLE          = no
+STENO_ENABLE         = no
+AUTOCORRECT_ENABLE   = no
+CAPS_WORD_ENABLE     = no
+KEY_OVERRIDE_ENABLE  = no
+UNICODE_ENABLE       = no
+UNICODEMAP_ENABLE    = no
 
-# QMKの便利系も削る
-MAGIC_ENABLE = no
-BOOTMAGIC_ENABLE = no          # 必要なら _LITE を yes に（それでも重ければ no のまま）
-SPACE_CADET_ENABLE = no
-GRAVE_ESC_ENABLE = yes
-TAP_DANCE_ENABLE = no
-LEADER_ENABLE = no
-COMBO_ENABLE = no
-UNICODE_ENABLE = no
-UNICODEMAP_ENABLE = no
-KEY_OVERRIDE_ENABLE = no
-CAPS_WORD_ENABLE = no
-STENO_ENABLE = no
-AUTOCORRECT_ENABLE = no
-
+MAGIC_ENABLE         = no
+BOOTMAGIC_ENABLE     = no        # BootMagic Lite も使わない前提
+SPACE_CADET_ENABLE   = no
+GRAVE_ESC_ENABLE     = no
+TAP_DANCE_ENABLE     = no
+LEADER_ENABLE        = no
+COMBO_ENABLE         = no
