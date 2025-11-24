da5d68579ea86aedab85c43cd1427ffb6a12d242
緒方 智行 <ogata.tomoyuki@gmail.com>
2025-11-24 12:10:09 +0900

    Update rules.mk

--- keyboards/keyboardio/atreus/keymaps/default/rules.mk
+++ keyboards/keyboardio/atreus/keymaps/default/rules.mk
@@ -1,17 +1,14 @@
-# Vial を使うので VIA はオフ
-VIA_ENABLE          = no
+# --- Vial 本体 ---
 VIAL_ENABLE         = yes
-LTO_ENABLE          = yes
+VIA_ENABLE          = no        # VIA は使わないので off 推奨
+EEPROM_ENABLE       = yes       # 設定を保存するため必須（通常はデフォルトで yes）
 
-# Vial のために必要
-EEPROM_ENABLE       = yes
-
-# 余計な設定画面はいらない
+# --- QMK Settings（Vial の Tap-Hold 設定タブを有効にする） ---
 QMK_SETTINGS        = yes
 
-# --- 必要なければ全部 no に（あとで必要になったら yes に変える） ---
-MOUSEKEY_ENABLE     = no   # マウスキー使うなら yes
-EXTRAKEY_ENABLE     = no   # メディアキーとかが欲しければ yes
+# --- 不要機能は切って容量を確保 ---
+MOUSEKEY_ENABLE     = no
+EXTRAKEY_ENABLE     = no
 CONSOLE_ENABLE      = no
 COMMAND_ENABLE      = no
 NKRO_ENABLE         = no
@@ -20,7 +17,7 @@ RGBLIGHT_ENABLE     = no
 AUDIO_ENABLE        = no
 UNICODE_ENABLE      = no
 UNICODEMAP_ENABLE   = no
-TAP_DANCE_ENABLE    = no
+TAP_DANCE_ENABLE    = no        # Tap Dance を使うなら yes に
 LEADER_ENABLE       = no
 COMBO_ENABLE        = no
 KEY_OVERRIDE_ENABLE = no
@@ -30,4 +27,4 @@ AUTOCORRECT_ENABLE  = no
 MAGIC_ENABLE        = no
 BOOTMAGIC_ENABLE    = no
 SPACE_CADET_ENABLE  = no
-GRAVE_ESC_ENABLE    = no   # 必要なら yes に
+GRAVE_ESC_ENABLE    = no        # 必要なら yes に
