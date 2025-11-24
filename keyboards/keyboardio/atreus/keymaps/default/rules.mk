bbbc7191ec31aec798aa4a8821c950294dec65a8
緒方 智行 <ogata.tomoyuki@gmail.com>
2025-11-24 13:02:34 +0900

    Configure VIAL settings and disable unused features
    Updated rules.mk to enable VIAL and disable unnecessary features for a lightweight configuration.

--- keyboards/keyboardio/atreus/keymaps/default/rules.mk
+++ keyboards/keyboardio/atreus/keymaps/default/rules.mk
@@ -1,35 +1,38 @@
- QMK_SETTINGS = yes
+# ----- Vial 基本設定 -----
+VIAL_ENABLE   = yes
+VIA_ENABLE    = no        # VIAL専用にするので VIA は不要
+EEPROM_ENABLE = yes       # Vial の設定保存に必要
 
-# --- 必要最小限 ---
-VIA_ENABLE = yes
-VIAL_ENABLE = yes
-LTO_ENABLE = yes
-EEPROM_ENABLE = yes
+# QMK_SETTINGS はフラッシュ容量的に無理なので OFF
+# （Tap/Hold の数値は config.h のマクロで固定）
+QMK_SETTINGS  = no
 
-# --- 重い/不要機能は全部OFF ---
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
-MOUSEKEY_ENABLE = yes
-EXTRAKEY_ENABLE = no
-NKRO_ENABLE = no
-BACKLIGHT_ENABLE = no
-RGBLIGHT_ENABLE = no
-RGB_MATRIX_ENABLE = no
-AUDIO_ENABLE = no
-MIDI_ENABLE = no
-SLEEP_LED_ENABLE = no
+# リンク時最適化（コードサイズ削減）
+LTO_ENABLE    = yes
 
-# QMKの便利系も削る
-MAGIC_ENABLE = no
-BOOTMAGIC_ENABLE = no          # 必要なら _LITE を yes に（それでも重ければ no のまま）
-SPACE_CADET_ENABLE = no
-GRAVE_ESC_ENABLE = yes
-TAP_DANCE_ENABLE = no
-LEADER_ENABLE = no
-COMBO_ENABLE = no
-UNICODE_ENABLE = no
-UNICODEMAP_ENABLE = no
-KEY_OVERRIDE_ENABLE = no
-CAPS_WORD_ENABLE = no
-STENO_ENABLE = no
-AUTOCORRECT_ENABLE = no
+# ----- 不要な機能を全てOFFにして軽量化 -----
+CONSOLE_ENABLE       = no
+COMMAND_ENABLE       = no
+MAGIC_ENABLE         = no
+BOOTMAGIC_ENABLE     = no
+SPACE_CADET_ENABLE   = no
+GRAVE_ESC_ENABLE     = no
+
+MOUSEKEY_ENABLE      = no
+EXTRAKEY_ENABLE      = no
+NKRO_ENABLE          = no
+BACKLIGHT_ENABLE     = no
+RGBLIGHT_ENABLE      = no
+RGB_MATRIX_ENABLE    = no
+AUDIO_ENABLE         = no
+MIDI_ENABLE          = no
+
+UNICODE_ENABLE       = no
+UNICODEMAP_ENABLE    = no
+STENO_ENABLE         = no
+AUTOCORRECT_ENABLE   = no
+CAPS_WORD_ENABLE     = no
+KEY_OVERRIDE_ENABLE  = no
+TAP_DANCE_ENABLE     = no
+LEADER_ENABLE        = no
+COMBO_ENABLE         = no
